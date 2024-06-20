329a45e2d54e12bbc7e21dbc4381a3a2881e74d9
rasaq-ola <olatech@DESKTOP-2QK9NGF>
2024-06-20 02:16:00 +0100

    API Advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,49 +1,20 @@
 #!/usr/bin/python3
-""" script to query Reddit API and
-return the number of users in given subreddit
 """
-from client_secrets import USERNAME, PASSWORD, CLIENT_ID, CLIENT_SECRET
-import json
+1-top_ten.py
+"""
 import requests
-import requests.auth
 
 
 def top_ten(subreddit):
-    """ return the  top 10 hot posts
-        in a given subreddit
-    """
-    # Authenticating Reddit app
-    client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
-    post_data = {
-        'grant_type': 'password',
-        'username': USERNAME,
-        'password': PASSWORD
-    }
-    headers = {
-        'User-Agent': 'Stephanie'
-    }
-
-    # Getting token Access Id
-    TOKEN_ACCESS_ENDPOINT = 'https://www.reddit.com/api/v1/access_token'
-    response = requests.post(TOKEN_ACCESS_ENDPOINT, data=post_data,
-                             headers=headers, auth=client_auth)
-    if response.status_code == 200:
-        token_id = response.json()['access_token']
-        headers['Authorization'] = f"bearer {token_id}"
-
-    # Query
-    OAUTH_ENDPOINT = "https://oauth.reddit.com"
-    params = {
-        'limit': 10
-    }
-    response = requests.get(OAUTH_ENDPOINT + f"/r/{subreddit}/hot",
-                            headers=headers, params=params,
-                            allow_redirects=False)
-
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'python:requests (by /u/yourusername)'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code != 200:
-        # print("ERRORR!", response.status_code)
-        print('None')
-        return 0
-
-    for each in response.json()['data']['children']:
-        print(each['data']['title'])
+        print(None)
+        return
+    
+    data = response.json().get('data', {}).get('children', [])
+    
+    for post in data:
+        print(post.get('data', {}).get('title'))
