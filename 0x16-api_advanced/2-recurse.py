b68a08d4f21e13fb4c3d5f2f571b811008063566
rasaq-ola <olatech@DESKTOP-2QK9NGF>
2024-06-21 01:34:54 +0100

    API-ADV.

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,20 +8,22 @@ import requests
 def recurse(subreddit, hot_list=[], after=None):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'python:requests (by /u/yourusername)'}
-    params = {'after': after}
-    
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
+    params = {'after': after, 'limit': 100}
+    response = requests.get(
+        url, headers=headers, params=params, allow_redirects=False
+    )
+
     if response.status_code != 200:
         return None
-    
+
     data = response.json().get('data', {})
-    after = data.get('after')
     children = data.get('children', [])
-    
+    after = data.get('after')
+
     for post in children:
-        hot_list.append(post.get('data', {}).get('title'))
-    
+        hot_list.append(post.get('data', {}).get('title', ''))
+
     if after is None:
         return hot_list
+
     return recurse(subreddit, hot_list, after)
