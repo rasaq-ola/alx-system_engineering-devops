026e47ff0f7b656f05f7cb774950be3dce756072
rasaq-ola <olatech@DESKTOP-2QK9NGF>
2024-06-21 01:43:27 +0100

    API-ADV.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,9 +9,21 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'python:requests (by /u/yourusername)'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code != 200:
         return 0
-    
-    data = response.json().get('data', {})
-    return data.get('subscribers', 0)
+
+    try:
+        data = response.json().get('data', {})
+        subscribers = data.get('subscribers', 0)
+        return subscribers
+    except ValueError:
+        return 0
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print(number_of_subscribers(sys.argv[1]))
